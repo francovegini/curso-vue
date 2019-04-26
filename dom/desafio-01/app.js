1255734e290925e1236f45bf9a2a4f4cf8f22d8a
Franco Vegini <francovegini@hotmail.com>
2019-04-25 21:48:23 -0300

    ajustes

--- dom/desafio-01/app.js
+++ dom/desafio-01/app.js
@@ -2,7 +2,7 @@ new Vue({
     el: '#desafio',
     data: {
         name: 'Franco Vegini',
-        age: '18',
+        age: 18,
         imageLink: 'https://www.torcedores.com/content/uploads/2017/10/torcida-flamengo-maracana-lotado.jpg'
     },
     methods: {
