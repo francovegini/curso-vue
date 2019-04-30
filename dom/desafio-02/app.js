78ab3405aa430a89124206765504f64a568fb4a1
Franco Vegini <francovegini@hotmail.com>
2019-04-29 21:04:42 -0300

    desafio 02

--- dom/desafio-02/app.js
+++ dom/desafio-02/app.js
@@ -1,6 +1,15 @@
 new Vue({
     el: '#desafio',
     data: {
-        valor: ''
+        valor: '',
+        valor2: ''
+    },
+    methods: {
+        mostrarAlerta() {
+            alert('Botao pressionado!')
+        },
+        armazenarValor(event) {
+            this.valor = event.target.value;
+        }
     }
 })
