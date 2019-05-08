36137f79b0ba0bca9ef5e7c5c4f6774bf1857a98
Franco Vegini <francovegini@hotmail.com>
2019-05-07 23:05:34 -0300

    desafio 044

--- dom/desafio-04/app.js
+++ dom/desafio-04/app.js
@@ -1,23 +1,38 @@
 new Vue({
     el: '#desafio',
     data: {
-        executar: false,
-        classeCss: 'c1',
+        classe1: 'destaque',
+        perigo: true,
+        classe3: '',
+        classe4: '',
+        cor5: '',
+        estilo5: {
+            width: '100px',
+            height: '100px',
+        },
+        width: '0',
     },
     methods: {
         iniciarEfeito() {
-            this.executar = !this.executar;
+            setInterval(() => {
+                this.classe1 = this.classe1 == 'destaque'
+                    ? 'encolher' : 'destaque'
+            }, 1000)
         },
         iniciarProgresso() {
-
-        }
-    },
-    computed: {
-        estilo1() {
-            return {
-                estilo1: true,
-                estilo2: true,
+            let valor = 0
+            const temporizador = setInterval(() => {
+                valor += 5
+                this.width = `${valor}%`
+                if (valor == 100) clearInterval(temporizador)
+            }, 500)
+        },
+        setPerigo(event) {
+            if (event.target.value == "true") {
+                this.perigo = true
+            } else if (event.target.value == "false") {
+                this.perigo = false
             }
         }
     }
-});
+})
