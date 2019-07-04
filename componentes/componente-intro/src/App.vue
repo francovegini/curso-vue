97048a15d4076583df878afdce242e1894a73607
Franco Vegini <francovegini@hotmail.com>
2019-07-03 23:55:26 -0300

    Criado componente com arquivo .vue

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <h1>Contadores</h1>
+        {{ contador }}
+        <button @click="contador++">+</button>
+        <button @click="contador++">-</button>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        el: "#app",
+        data() {
+            return {
+                contador: 0
+            };
+        }
+    };
+</script>
