e0e8cddebc0ad180fff764dacad76893b0f7cc58
Franco Vegini <francovegini@hotmail.com>
2019-07-08 22:41:27 -0300

    separando em componentes

--- componentes/componente-desafio/src/App.vue
+++ componentes/componente-desafio/src/App.vue
@@ -25,44 +25,31 @@
 			</v-list>
 		</v-navigation-drawer>
 
-		<v-toolbar app>
-			<v-toolbar-title class="headline text-uppercase">
-				<span>Desafio</span>
-				<span class="font-weight-light">Componente</span>
-			</v-toolbar-title>
-		</v-toolbar>
-
-		<v-content>
-			<v-carousel>
-				<v-carousel-item
-					v-for="(item,i) in fotos"
-					:key="i"
-					:src="item.src"></v-carousel-item>
-			</v-carousel>
-		</v-content>
-
-		<v-footer class="pa-3" app>
-			<v-spacer></v-spacer>
-			<div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
-		</v-footer>
+		<app-toolbar/>
+		<app-carousel/>
+		<app-footer/>
 	</v-app>
 </template>
 
 <script>
+
+	import Footer from "./Footer";
+	import Carousel from "./Carousel";
+	import Toolbar from "./Toolbar"
+
 export default {
 	data() {
 		return {
 			itensMenu: [
 				{titulo: 'Início', icon: 'dashboard'},
 				{titulo: 'Sobre', icon: 'question_answer'}
-			],
-			fotos: [
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
 			]
 		}
+	},
+	components: {
+		"app-footer": Footer,
+		"app-carousel": Carousel,
+		"app-toolbar": Toolbar
 	}
 }
 </script>
