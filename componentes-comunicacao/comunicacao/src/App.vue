bb14385750ea36d5966fa6ff3c6546949ebaf073
Franco Vegini <francovegini@hotmail.com>
2019-07-15 22:48:10 -0300

    Comunicação entre componentes

--- componentes-comunicacao/comunicacao/src/App.vue
+++ componentes-comunicacao/comunicacao/src/App.vue
@@ -1,38 +1,38 @@
 <template>
-	<div id="app">
-		<app-usuario />
-	</div>
+    <div id="app">
+        <app-usuario/>
+    </div>
 </template>
 
 <script>
-import AppUsuario from "./components/Usuario.vue";
+    import AppUsuario from "./components/Usuario.vue";
 
-export default {
-	name: "app",
-	components: { AppUsuario }
-}
+    export default {
+        name: "app",
+        components: {AppUsuario}
+    };
 </script>
 
 <style>
-#app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 1.7rem;
-}
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+        font-size: 1.7rem;
+    }
 
-h1, h2, p {
-	margin: 10px;
-	padding: 0px;
-}
+    h1, h2, p {
+        margin: 10px;
+        padding: 0px;
+    }
 
-button {
-	font-size: 1.5rem;
-	padding: 5px 15px;
-	border-radius: 4px;
-	margin: 10px;
-}
+    button {
+        font-size: 1.5rem;
+        padding: 5px 15px;
+        border-radius: 4px;
+        margin: 10px;
+    }
 </style>
