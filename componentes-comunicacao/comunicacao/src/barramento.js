b93678cdd0914d0c894fdfcc84fef3020683b094
Franco Vegini <francovegini@hotmail.com>
2019-07-25 23:16:13 -0300

    Adicionado metodos no evento bus

--- componentes-comunicacao/comunicacao/src/barramento.js
+++ componentes-comunicacao/comunicacao/src/barramento.js
@@ -1,2 +1,12 @@
-import Vue from 'vue'
-export default new Vue()
+import Vue from "vue";
+
+export default new Vue({
+    methods: {
+        alterarIdade(idade) {
+            this.$emit("idadeMudou", idade);
+        },
+        quandoIdadeMudar(callback) {
+            this.$on("idadeMudou", callback);
+        }
+    }
+});
