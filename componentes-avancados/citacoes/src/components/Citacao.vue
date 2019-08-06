ceb255ae5ade5bb5b1b6315276edd9354dd2d5ac
Franco Vegini <francovegini@hotmail.com>
2019-08-06 20:57:09 -0300

    explorando projeto

--- componentes-avancados/citacoes/src/components/Citacao.vue
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="citacao">
-        
+
     </div>
 </template>
 
 <script>
-export default {
-
-}
+    export default {};
 </script>
 
 <style scoped>
--- componentes-avancados/citacoes/src/main.js
+++ componentes-avancados/citacoes/src/main.js
@@ -1,9 +1,9 @@
-import './estilo.css'
-import Vue from 'vue'
-import App from './App.vue'
+import "./estilo.css";
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount("#app");
