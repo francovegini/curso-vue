919ca5921ac02dcc4564437e41054c1bdb364957
Franco Vegini <francovegini@hotmail.com>
2019-09-12 21:54:21 -0300

    Submetendo o form

--- formularios/formulario-exercicios/src/App.vue
+++ formularios/formulario-exercicios/src/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <h1>Registrar Reclamação</h1>
         <div class="conteudo">
-            <form class="painel">
+            <form class="painel" v-if="!enviado">
                 <div class="cabecalho">Formulário</div>
                 <Rotulo nome="E-mail">
                     <input type="text" v-model.lazy.trim="usuario.email">
@@ -49,9 +49,9 @@
                     <Escolha v-model="escolha"/>
                 </Rotulo>
                 <hr>
-                <button>Enviar</button>
+                <button @click.prevent="enviar">Enviar</button>
             </form>
-            <div class="painel">
+            <div class="painel" v-else>
                 <div class="cabecalho">Resultado</div>
                 <Rotulo nome="E-mail">
                     <span>{{ usuario.email }}</span>
@@ -113,7 +113,13 @@
                     senha: '',
                     idade: 25
                 },
-                escolha: true
+                escolha: true,
+                enviado: false
+            }
+        },
+        methods: {
+            enviar() {
+                this.enviado = true;
             }
         }
     }
