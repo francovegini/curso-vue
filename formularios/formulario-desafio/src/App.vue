aa04f833edf7ce0c27ea16f134466346d1b45f82
Franco Vegini <francovegini@hotmail.com>
2019-09-15 23:12:18 -0300

    desafio

--- formularios/formulario-desafio/src/App.vue
+++ formularios/formulario-desafio/src/App.vue
@@ -1,96 +1,148 @@
 <template>
-	<div id="app">
-		<h1>Formulário Desafio</h1>
-		<div class="conteudo">
-			<form class="painel">
-				<div class="cabecalho">Formulário</div>
-				<!-- Exercicio 01 -->
-				<!-- Criar uma formulário de registro -->
-				<!-- Nome completo (Nome e Sobrenome) -->
-				<!-- Email -->
-				<!-- Senha -->
-				<!-- Armazenar Dados? (Sim/Não) -->
+    <div id="app">
+        <h1>Formulário Desafio</h1>
+        <div class="conteudo">
+            <form class="painel" v-if="!enviado">
+                <div class="cabecalho">Formulário</div>
+                <!-- Exercicio 01 -->
+                <!-- Criar uma formulário de registro -->
+                <!-- Nome completo (Nome e Sobrenome) -->
+                <!-- Email -->
+                <!-- Senha -->
+                <!-- Armazenar Dados? (Sim/Não) -->
+                <Rotulo nome="Nome">
+                    <input type="text" v-model.trim="dados.nome">
+                </Rotulo>
+                <Rotulo nome="Sobrenome">
+                    <input type="text" v-model.trim="dados.sobrenome">
+                </Rotulo>
+                <Rotulo nome="E-mail">
+                    <input type="text" v-model.trim="dados.email">
+                </Rotulo>
+                <Rotulo nome="Senha">
+                    <input type="password" v-model="dados.senha">
+                </Rotulo>
+                <Rotulo nome="Armazenar dados?">
+					<span class="mr-4">
+                        <input type="radio" value="true" v-model="dados.armazenarDados"> Sim
+                    </span>
+                    <span>
+                        <input type="radio" value="false" v-model="dados.armazenarDados"> Não
+                    </span>
+                </Rotulo>
+                <NomeCompleto></NomeCompleto>
+                <hr>
+                <button @click.prevent="enviar">Enviar</button>
 
-				<!-- Exercicio 02 -->
-				<!-- Só mostrar o fomulário de não tiver sido submetido -->
-				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
-				
-				<!-- Exercicio 03 -->
-				<!-- Crie um componente personalizado NomeCompleto -->
-				<!-- Esse componente deve receber Nome e Sobrenome -->
-			</form>
-			<div class="painel">
-				<div class="cabecalho">Resultado</div>
+                <!-- Exercicio 02 -->
+                <!-- Só mostrar o fomulário de não tiver sido submetido -->
+                <!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
 
-			</div>
-		</div>
-	</div>
+                <!-- Exercicio 03 -->
+                <!-- Crie um componente personalizado NomeCompleto -->
+                <!-- Esse componente deve receber Nome e Sobrenome -->
+            </form>
+            <div class="painel" v-else>
+                <div class="cabecalho">Resultado</div>
+                <Rotulo nome="Nome completo">
+                    <span>{{ dados.nome }}  {{dados.sobrenome}}</span>
+                </Rotulo>
+                <Rotulo nome="E-mail">
+                    <span>{{ dados.email }}</span>
+                </Rotulo>
+                <Rotulo nome="Senha">
+                    <span>{{ dados.senha }}</span>
+                </Rotulo>
+                <Rotulo nome="Armazenar dados?">
+                    <span>{{ dados.armazenarDados }}</span>
+                </Rotulo>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import Rotulo from './components/Rotulo.vue'
+    import Rotulo from './components/Rotulo.vue';
+    import NomeCompleto from './components/NomeCompleto';
 
-export default {
-	name: 'app',
-	components: { Rotulo }
-}
+    export default {
+        name: 'app',
+        components: { Rotulo, NomeCompleto },
+        data() {
+            return {
+                dados: {
+                    nome: '',
+                    sobrenome: '',
+                    email: '',
+                    senha: '',
+                    armazenarDados: true
+                },
+                enviado: false
+            }
+        },
+        methods: {
+            enviar() {
+                this.enviado = true;
+            }
+        }
+    }
 </script>
 
 <style>
 
-body {
-	background-color: #ECECEC;
-}
+    body {
+        background-color: #ECECEC;
+    }
 
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
 
-	display: flex;
-	flex-direction: column;
-}
+        display: flex;
+        flex-direction: column;
+    }
 
-.conteudo {
-	display: flex;
-}
+    .conteudo {
+        display: flex;
+    }
 
-.painel {
-	flex: 1;
-	background: #FFF;
-	margin: 0px 10px;
-	padding: 20px;
-	border: 1px solid #AAA;
-	border-radius: 5px;
-}
+    .painel {
+        flex: 1;
+        background: #FFF;
+        margin: 0px 10px;
+        padding: 20px;
+        border: 1px solid #AAA;
+        border-radius: 5px;
+    }
 
-.painel .cabecalho {
-	width: 100%;
-	background-color: #DDD;
-	padding: 10px 0px;
-	border-radius: 5px;
-	font-size: 1.4rem;
-}
+    .painel .cabecalho {
+        width: 100%;
+        background-color: #DDD;
+        padding: 10px 0px;
+        border-radius: 5px;
+        font-size: 1.4rem;
+    }
 
-#app form button {
-	float: right;
-	margin: 10px 0px;
-	padding: 10px 20px;
-	font-size: 1.4rem;
-	border-radius: 5px;
-	color: #FFF;
-	background-color: #2196F3;
-}
+    #app form button {
+        float: right;
+        margin: 10px 0px;
+        padding: 10px 20px;
+        font-size: 1.4rem;
+        border-radius: 5px;
+        color: #FFF;
+        background-color: #2196F3;
+    }
 
-#app h1 {
-	font-weight: 200;
-	margin: 20px;
-	padding: 0;
-}
+    #app h1 {
+        font-weight: 200;
+        margin: 20px;
+        padding: 0;
+    }
 
-.mr-4 {
-	margin-right: 40px;
-}
+    .mr-4 {
+        margin-right: 40px;
+    }
 </style>
---
+++ formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="nome-completo">
+        <Rotulo nome="Nome">
+            <input type="text" v-model.trim="nome">
+        </Rotulo>
+        <Rotulo nome="Sobrenome">
+            <input type="text" v-model.trim="sobrenome">
+        </Rotulo>
+    </div>
+</template>
+
+<script>
+    import Rotulo from './Rotulo';
+
+    export default {
+        name: "NomeCompleto",
+        components: { Rotulo },
+        data() {
+            return {
+                nome: '',
+                sobrenome: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
