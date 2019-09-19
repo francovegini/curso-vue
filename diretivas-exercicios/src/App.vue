8d82ae5041ce2c4a806cd69f960bb355d4d9abd2
Franco Vegini <francovegini@hotmail.com>
2019-09-18 21:29:48 -0300

    ajustes

--- diretivas-exercicios/src/App.vue
+++ diretivas-exercicios/src/App.vue
@@ -1,23 +1,21 @@
 <template>
-	<div id="app">
-		<h1>Diretivas</h1>
-	</div>
+    <div id="app">
+        <h1>Diretivas</h1>
+    </div>
 </template>
 
 <script>
-export default {
-	
-}
+    export default {}
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 2.5rem;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+        font-size: 2.5rem;
+    }
 </style>
