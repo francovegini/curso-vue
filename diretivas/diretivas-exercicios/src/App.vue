6a2b80f8cd4775d0b1e3bc7f22c3084ab71f7463
Franco Vegini <francovegini@hotmail.com>
2019-09-30 21:22:40 -0300

    Usando multiplos modificadores

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -10,7 +10,7 @@
         <p v-destaque2:fundo.atrasar="'lightblue'">Usando diretiva personalizada com parâmetro</p>
 
         <hr>
-        <p v-destaque-local:fundo.atrasar="'lightblue'">Usando diretiva personalizada com parâmetro</p>
+        <p v-destaque-local.atrasar.alternar="'lightblue'">Usando diretiva personalizada com parâmetro</p>
     </div>
 </template>
 
@@ -19,16 +19,31 @@
         directives: {
             'destaque-local': {
                 bind(el, binding) {
+                    const aplicarCor = cor => {
+                        if (binding.arg === 'fundo') {
+                            el.style.backgroundColor = cor;
+                        } else {
+                            el.style.color = cor;
+                        }
+                    };
+
                     let atraso = 0;
                     if (binding.modifiers['atrasar']) {
                         atraso = 3000;
                     }
 
+                    const cor1 = binding.value;
+                    const cor2 = 'purple';
+                    let corAtual = cor1;
+
                     setTimeout(() => {
-                        if (binding.arg === 'fundo') {
-                            el.style.backgroundColor = binding.value;
+                        if (binding.modifiers['alternar']) {
+                            setInterval(() => {
+                                corAtual = corAtual === cor1 ? cor2 : cor1;
+                                aplicarCor(corAtual);
+                            }, 1000)
                         } else {
-                            el.style.color = binding.value;
+                            aplicarCor(binding.value)
                         }
                     }, atraso)
                 }
