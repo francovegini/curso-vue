739216e1c7bc7b39eaa676f952b325bc16f71a8b
Franco Vegini <francovegini@hotmail.com>
2020-01-23 16:03:23 -0200

    projeto http-exercicios

---
+++ http/http-exercicios/src/plugins/bootstrap-vue.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+import BootstrapVue from 'http/http-exercicios/src/plugins/bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
