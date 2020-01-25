76131d7273d0485dda77bd823bda6aeba4e878f3
Franco Vegini <francovegini@hotmail.com>
2020-01-25 16:27:15 -0200

    ajustado import

--- http/http-exercicios/src/plugins/bootstrap-vue.js
+++ http/http-exercicios/src/plugins/bootstrap-vue.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-import BootstrapVue from 'http/http-exercicios/src/plugins/bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
