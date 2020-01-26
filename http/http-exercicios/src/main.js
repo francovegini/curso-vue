f2d9515dddd14f5402638030b1d6f547eb43ce8a
Franco Vegini <francovegini@hotmail.com>
2020-01-26 16:39:38 -0200

    Criado plugin Axios

--- http/http-exercicios/src/main.js
+++ http/http-exercicios/src/main.js
@@ -3,9 +3,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import './plugins/bootstrap-vue'
+import './plugins/axios'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
