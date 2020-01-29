903b98999a66cc481f178df791cdc0979f91a0b0
Franco Vegini <francovegini@hotmail.com>
2020-01-29 00:32:46 -0200

    Criando formulário

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -1,32 +1,64 @@
 <template>
-	<div id="app" class="container">
-		<h1>HTTP com Axios</h1>
-	</div>
+    <div id="app" class="container">
+        <h1>HTTP com Axios</h1>
+        <b-card>
+            <b-form-group label="Nome:">
+                <b-form-input type="text" size="lg"
+                              v-model="usuario.nome"
+                              placeholder="Informe o nome">
+                </b-form-input>
+            </b-form-group>
+            <b-form-group label="Email:">
+                <b-form-input type="email" size="lg"
+                              v-model="usuario.email"
+                              placeholder="Informe o email">
+                </b-form-input>
+            </b-form-group>
+            <hr>
+            <b-button @click="salvar" size="lg" variant="primary">Salvar
+            </b-button>
+
+        </b-card>
+    </div>
 </template>
 
 <script>
-export default {
-	created() {
-		// Teste axios
-		this.$http.post('usuarios.json', {
-			nome: 'Franco',
-			email: 'francovegini@hotmail.com'
-		}).then(res => console.log(res));
-	}
-}
+    export default {
+        data() {
+            return {
+                usuario: {
+                    nome: '',
+                    email: ''
+                }
+            }
+        },
+		methods: {
+        	salvar() {
+        		console.log(this.usuario)
+			}
+		}
+
+        // created() {
+        // 	// Teste axios
+        // 	this.$http.post('usuarios.json', {
+        // 		nome: 'Franco',
+        // 		email: 'francovegini@hotmail.com'
+        // 	}).then(res => console.log(res));
+        // }
+    }
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-	font-size: 1.5rem;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+        font-size: 1.5rem;
+    }
 
-#app h1 {
-	text-align: center;
-	margin: 50px;
-}
+    #app h1 {
+        text-align: center;
+        margin: 50px;
+    }
 </style>
