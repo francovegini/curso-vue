eda49b6857744447262a6efca61bab8f60eeec2a
Franco Vegini <francovegini@hotmail.com>
2020-02-03 00:28:26 -0200

    ajustes na aspas

--- http/http-exercicios/src/plugins/axios.js
+++ http/http-exercicios/src/plugins/axios.js
@@ -1,14 +1,14 @@
-import Vue from 'vue';
-import axios from 'axios';
+import Vue from "vue";
+import axios from "axios";
 import Axios from "axios";
 
-// axios.defaults.baseURL = 'https://curso-vue-312f1.firebaseio.com/';
+// axios.defaults.baseURL = "https://curso-vue-312f1.firebaseio.com/";
 
 Vue.use({
     install(Vue){
         // Vue.prototype.$http = axios;
         Vue.prototype.$http = Axios.create({
-            baseURL: 'https://curso-vue-312f1.firebaseio.com/'
+            baseURL: "https://curso-vue-312f1.firebaseio.com/"
         })
     }
 })
