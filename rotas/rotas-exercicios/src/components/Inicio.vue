38a599b91612dff1951c6be2a3f0d79fb36d4aa5
Franco Vegini <francovegini@hotmail.com>
2020-02-19 21:42:12 -0300

    package

---
+++ rotas/rotas-exercicios/src/components/Inicio.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Inicio"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Usuario"
+    }
+</script>
+
+<style scoped>
+
+</style>
