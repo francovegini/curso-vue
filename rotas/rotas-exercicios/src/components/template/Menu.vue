03f1b814c8e649ca1db5fe8344eab42158bfac5b
Franco Vegini <francovegini@hotmail.com>
2020-02-26 22:11:13 -0300

    Configurando parametros nas rotas

--- rotas/rotas-exercicios/src/components/template/Menu.vue
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -3,7 +3,7 @@
         <ul>
             <router-link to="/" tag="li" active-class="active" exact>
                 <a>Início</a></router-link>
-            <router-link to="/usuario" tag="li" active-class="active">
+            <router-link to="/usuario/12" tag="li" active-class="active">
                 <a>Usuário</a></router-link>
         </ul>
     </nav>
