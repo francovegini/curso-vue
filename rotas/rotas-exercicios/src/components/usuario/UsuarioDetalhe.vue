52aa154fbfcc51948d398c95a9191748062f6997
Franco Vegini <francovegini@hotmail.com>
2020-03-02 17:32:19 -0300

    Criado componente UsuarioDetalhe

--- rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -1,10 +1,25 @@
 <template>
-    $END$
+    <div class="usuario-detalhe">
+        <h3>Usuário Detalhe</h3>
+        <hr>
+        <p><strong>Código:</strong> {{ id }}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "UsuarioDetalhe"
+        name: "UsuarioDetalhe",
+        props: ['id'],
+        // data() {
+        //     return {
+        //         id: this.$route.params.id
+        //     }
+        // },
+        // watch: {
+        //     $route(to, from){
+        //         this.id = to.params.id;
+        //     }
+        // },
     }
 </script>
 
