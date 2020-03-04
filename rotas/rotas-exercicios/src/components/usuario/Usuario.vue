f2cf7980b7109e7967c353348165e0ddf7607174
Franco Vegini <francovegini@hotmail.com>
2020-03-04 17:33:24 -0300

    Ajustado router.js para rotas filhas e ajustado componente Usuario

--- rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,7 +2,7 @@
     <div class="usuario">
         <h2>Usuários</h2>
         <hr>
-        <p><strong>Código:</strong> {{ id }}</p>
+        <router-view/>
         <button sucesso @click="irParaInicio">Voltar</button>
     </div>
 </template>
@@ -10,17 +10,6 @@
 <script>
     export default {
         name: "Usuario",
-        props: ['id'],
-        // data() {
-        //     return {
-        //         id: this.$route.params.id
-        //     }
-        // },
-        // watch: {
-        //     $route(to, from){
-        //         this.id = to.params.id;
-        //     }
-        // },
         methods: {
             irParaInicio() {
                 this.$router.push('/');
