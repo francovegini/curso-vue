172fa9523dd3dafb24d58a6c8b01d487f2826187
Franco Vegini <francovegini@hotmail.com>
2020-03-09 12:51:51 -0300

    Usando parametros da query

--- rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -3,7 +3,8 @@
         <h3>Usuário Detalhe</h3>
         <p><strong>Código:</strong> {{ id }}</p>
         <!-- :to="`/usuario/${id}/editar`" -->
-        <router-link tag="button" primario :to="{ name: 'editarUsuario', params: { id } }">
+        <router-link tag="button" primario :to="{ name: 'editarUsuario', params: { id },
+                                                  query: { completo: true, lingua: 'pt'} }">
             Editar
         </router-link>
     </div>
