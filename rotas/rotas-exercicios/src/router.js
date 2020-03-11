8567f25c4c0a13f355ca76941d7388d1ef1aedc2
Franco Vegini <francovegini@hotmail.com>
2020-03-11 13:13:45 -0300

    Redirecionamento de um path específico

--- rotas/rotas-exercicios/src/router.js
+++ rotas/rotas-exercicios/src/router.js
@@ -35,6 +35,10 @@ export default new Router({
                 { path: ':id', component: UsuarioDetalhe, props: true },
                 { path: ':id/editar', component: UsuarioEditar, props: true, name: 'editarUsuario' }
             ]
+        },
+        {
+            path: '/redirecionar',
+            redirect: '/usuario'
         }
     ]
 })
