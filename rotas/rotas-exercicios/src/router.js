2b0f040b070c4c0e9a037fd6c682e0f20745c95f
Franco Vegini <francovegini@hotmail.com>
2020-03-12 13:14:50 -0300

    Redirecionamento para todos paths não existentes no sistema

--- rotas/rotas-exercicios/src/router.js
+++ rotas/rotas-exercicios/src/router.js
@@ -39,6 +39,10 @@ export default new Router({
         {
             path: '/redirecionar',
             redirect: '/usuario'
+        },
+        {
+            path: '*',
+            redirect: '/'
         }
     ]
 })
