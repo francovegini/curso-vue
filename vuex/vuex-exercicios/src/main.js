fe817c762f8ad66a1cce42f1a7560a5220909563
Franco Vegini <francovegini@hotmail.com>
2020-03-25 15:46:56 -0300

    Configurado store na aplicacao

--- vuex/vuex-exercicios/src/main.js
+++ vuex/vuex-exercicios/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Painel from './components/Painel'
+import store from './store/store'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ Vue.filter('dinheiro', valor => {
 })
 
 new Vue({
+    store,
     render: h => h(App),
 }).$mount('#app')
