6e64471dac293618a79a3dac7a061c838a7ff90e
Franco Vegini <francovegini@hotmail.com>
2020-05-01 19:45:29 -0300

    Ajustado componente Stocks

--- projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
@@ -1,10 +1,25 @@
 <template>
-    <h1>Ações</h1>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id"/>
+    </v-layout>
 </template>
 
 <script>
+import Stock from './Stock'
+
 export default {
-    name: "Stocks"
+    name: "Stocks",
+    components: { Stock },
+    data() {
+        return {
+            stocks: [
+                { id: 1, name: 'BMW', price: 70 },
+                { id: 2, name: 'Google', price: 250 },
+                { id: 3, name: 'Apple', price: 190 },
+                { id: 4, name: 'Twitter', price: 170 },
+            ]
+        }
+    }
 }
 </script>
 
