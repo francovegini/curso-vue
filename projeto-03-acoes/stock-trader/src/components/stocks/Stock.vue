4f7900c48c8cfa61190e5ef42a84faadb7d4a6c5
Franco Vegini <francovegini@hotmail.com>
2020-05-03 16:03:26 -0300

    Ajustado quantity das ações

--- projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
@@ -7,7 +7,8 @@
         </v-card>
         <v-card>
             <v-container fill-height>
-                <v-text-field label="Quantidade" type="number"/>
+                <v-text-field label="Quantidade" type="number"
+                              v-model.number="quantity"/>
                 <v-btn class="green darken-3 white--text">Comprar</v-btn>
             </v-container>
         </v-card>
@@ -17,7 +18,12 @@
 <script>
 export default {
     name: "Stock",
-    props: ["stock"]
+    props: ["stock"],
+    data() {
+        return {
+            quantity: 0
+        }
+    }
 }
 </script>
 
