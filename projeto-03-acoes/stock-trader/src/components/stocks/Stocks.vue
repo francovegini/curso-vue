ed3a151adbbae865ce2a9a0e5b61198162087f78
Franco Vegini <francovegini@hotmail.com>
2020-05-10 16:30:53 -0300

    Removido dados do componente e passado para um arquivo data (simulando backend)

--- projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
@@ -9,20 +9,9 @@ import Stock from './Stock'
 
 export default {
     name: "Stocks",
-    components: { Stock },
-    data() {
-        return {
-            stocks: [
-                { id: 1, name: 'BMW', price: 70 },
-                { id: 2, name: 'Google', price: 250 },
-                { id: 3, name: 'Apple', price: 190 },
-                { id: 4, name: 'Twitter', price: 170 },
-            ]
-        }
-    }
+    components: { Stock }
 }
 </script>
 
 <style>
-
 </style>
---
+++ projeto-03-acoes/stock-trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: 'BMW', price: 70 },
+    { id: 2, name: 'Google', price: 250 },
+    { id: 3, name: 'Apple', price: 190 },
+    { id: 4, name: 'Twitter', price: 170 },
+]
