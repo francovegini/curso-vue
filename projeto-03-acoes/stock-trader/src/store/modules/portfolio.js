319a2e1fcb54fb35b282935cda08354c5e1e388d
Franco Vegini <francovegini@hotmail.com>
2020-05-13 17:12:17 -0300

    Implementado buyStock no portfolio store

---
+++ projeto-03-acoes/stock-trader/src/store/modules/portfolio.js
@@ -0,0 +1,21 @@
+export default {
+    state: {
+        funds: 10000,
+        stocks: []
+    },
+    mutations: {
+        buyStock(state, { stockId, quantity, stockPrice }) {
+            const record = state.stocks.find(e => e.id == stockId);
+
+            if (record) {
+                record.quantity += quantity;
+            } else {
+                state.stocks.push({
+                    id: stockId,
+                    quantity: quantity
+                });
+            }
+            state.funds -= stockPrice * quantity;
+        }
+    }
+}
