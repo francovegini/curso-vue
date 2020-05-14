7a7f6a1d1bc57ae5929f8878da10cc6e35db9ed7
Franco Vegini <francovegini@hotmail.com>
2020-05-14 17:18:40 -0300

    Criado mutation sellStock()

--- projeto-03-acoes/stock-trader/src/store/modules/portfolio.js
+++ projeto-03-acoes/stock-trader/src/store/modules/portfolio.js
@@ -16,6 +16,16 @@ export default {
                 });
             }
             state.funds -= stockPrice * quantity;
+        },
+        sellStock(state, { stockId, quantity, stockPrice }){
+            const record = state.stocks.find(e => e.id == stockId);
+
+            if(record.quantity > quantity){
+                record.quantity -= quantity;
+            } else {
+                state.stocks.splice(state.stocks.indexOf(record), 1)
+            }
+            state.funds += stockPrice * quantity;
         }
     }
 }
