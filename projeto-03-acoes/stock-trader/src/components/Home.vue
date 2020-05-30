a82624cce52ce99a0d2729fbebe5c6cf19185060
Franco Vegini <francovegini@hotmail.com>
2020-05-30 19:28:33 -0300

    ajustes css

--- projeto-03-acoes/stock-trader/src/components/Home.vue
+++ projeto-03-acoes/stock-trader/src/components/Home.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
-        <h1>Negocie e consulte suas ações</h1>
-        <v-sheet color="primary">
-            <v-icon>info</v-icon>
+        <h1 class="display-3 font-weight-light mb-4">Negocie e consulte suas ações</h1>
+        <v-sheet :elevation="6" class="pa-2" color="primary">
+            <v-icon class="white--text mr-3">info</v-icon>
             <span>Você pode salvar & carregar os dados</span>
         </v-sheet>
         <v-divider class="my-4"/>
         <p><strong>Seu saldo:</strong> {{ funds }}</p>
-
     </div>
 </template>
 
