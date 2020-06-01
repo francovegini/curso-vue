899b121a7f67df8cbb4859580db2267cfb9b0905
Franco Vegini <francovegini@hotmail.com>
2020-06-01 19:33:13 -0300

    finalizado componente

--- projeto-03-acoes/stock-trader/src/components/Home.vue
+++ projeto-03-acoes/stock-trader/src/components/Home.vue
@@ -3,7 +3,9 @@
         <h1 class="display-3 font-weight-light mb-4">Negocie e consulte suas ações</h1>
         <v-sheet :elevation="6" class="pa-2 green">
             <v-icon class="white--text mr-3">info</v-icon>
-            <span>Você pode salvar & carregar os dados</span>
+            <span class="headline white--text font-weight-light">
+                Você pode salvar & carregar os dados
+            </span>
         </v-sheet>
         <v-divider class="my-4"/>
         <p><strong>Seu saldo:</strong> {{ funds }}</p>
