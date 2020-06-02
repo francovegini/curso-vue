a3a83ba28e2578a88450ec4af233cbc0e32c55d1
Franco Vegini <francovegini@hotmail.com>
2020-06-02 19:35:36 -0300

    duplicado template

--- projeto-03-acoes/stock-trader/src/components/Home.vue
+++ projeto-03-acoes/stock-trader/src/components/Home.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
         <h1 class="display-3 font-weight-light mb-4">Negocie e consulte suas ações</h1>
-        <v-sheet :elevation="6" class="pa-2 green">
+        <v-sheet :elevation="6" class="pa-2 primary">
             <v-icon class="white--text mr-3">info</v-icon>
             <span class="headline white--text font-weight-light">
                 Você pode salvar & carregar os dados
+            </span>
+        </v-sheet>
+        <v-sheet :elevation="6" class="pa-2 success darken-1 mt-3">
+            <v-icon class="white--text mr-3">info</v-icon>
+            <span class="headline white--text font-weight-light">
+                Clique em 'Finalizar Dia' para iniciar um novo dia!
             </span>
         </v-sheet>
         <v-divider class="my-4"/>
