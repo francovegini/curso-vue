3d721f48768125648c07d17f83a85c76530b8f81
francovegini <francovegini@hotmail.com>
2020-09-17 21:21:55 -0300

    css

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -10,6 +10,29 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #FFF;
+  background: linear-gradient(90deg, rgba(104,101,168,1) 0%, rgba(59,140,157,1) 100%);
+}
 
 </style>
