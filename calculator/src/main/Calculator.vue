0854cef9ed46142d39612292205547458acd1e0c
francovegini <francovegini@hotmail.com>
2020-09-18 23:37:56 -0300

    Calculator.vue

---
+++ calculator/src/main/Calculator.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="calculator">
+    <Display/>
+    <Button/>
+  </div>
+</template>
+
+<script>
+import Display from "@/components/Display";
+import Button from "@/components/Button";
+
+export default {
+  name: "Calculator",
+  components: { Button, Display }
+}
+</script>
+
+<style>
+</style>
