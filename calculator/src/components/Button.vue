9c3f20b90f1c2ecdea524eebfc8ee3a5824feacf
francovegini <francovegini@hotmail.com>
2020-10-12 17:40:59 -0300

    Componente Button

--- calculator/src/components/Button.vue
+++ calculator/src/components/Button.vue
@@ -1,15 +1,34 @@
 <template>
-  <button class="button">
-    0
+  <button class="button" :class="{ double, trilpe, operation }">
+    {{ label }}
   </button>
 </template>
 
 <script>
 export default {
-  name: "Button"
+  name: "Button",
+  props: {
+    label: {},
+    operation: { type: Boolean },
+    double: { type: Boolean },
+    triple: { type: Boolean },
+  }
 }
 </script>
 
 <style>
+:root {
+  --bg-button: #f0f0f0;
+  --border-button: solid 1px #888;
+}
+
+.button {
+  font-size: 1.4em;
+  background-color: var(--bg-button);
+  border: none;
+  border-right: var(--border-button);
+  border-bottom: var(--border-button);
+  outline: none;
+}
 
 </style>
