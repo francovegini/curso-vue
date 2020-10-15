e989fe6d3449132bb539f25f30b734cb92f9f9a2
francovegini <francovegini@hotmail.com>
2020-10-15 00:51:15 -0300

    setOperation

--- calculator/src/main/Calculator.vue
+++ calculator/src/main/Calculator.vue
@@ -42,7 +42,27 @@ export default {
       Object.assign(this.$data, this.$options.data());
     },
     setOperation(operation) {
-      console.log("operation" + operation);
+      if (this.current === 0) {
+        this.operation = operation;
+        this.current = 1;
+        this.clearDisplay = true;
+      } else {
+        const equals = operation === "=";
+        const currentOperation = this.operation;
+
+        try {
+          this.values[0] = eval(`${ this.values[0] } ${ currentOperation } ${ this.values[1] }`);
+        } catch (e) {
+          this.emit('onError', e);
+        }
+
+        this.values[1] = 0;
+
+        this.displayValue = this.values[0]
+        this.operation = equals ? null : operation;
+        this.current = equals ? 0 : 1;
+        this.clearDisplay = !equals;
+      }
     },
     addDigit(n) {
       if (n === "." && this.displayValue.includes(".")) {
