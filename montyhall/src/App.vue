13fbc94c1a2ea7314f4f41645fdceee147d95367
francovegini <francovegini@hotmail.com>
2020-12-16 22:05:27 -0300

    Estrutura do formulario

--- montyhall/src/App.vue
+++ montyhall/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <h1>Problema de Monty Hall</h1>
-    <Door number="1" :has-gift="false"/>
+    <div class="form">
+      <div v-if="!started">
+        <label for="doorsAmount">Quantas portas?</label>
+        <input type="text" id="doorsAmount" size="3" v-model.number="doorsAmount">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +15,14 @@ import Door from "./components/Door";
 
 export default {
   name: "App",
-  components: { Door }
+  components: { Door },
+  data: function () {
+    return {
+      started: false,
+      doorsAmount: 3,
+      selectedDoor: null
+    }
+  }
 }
 </script>
 
