81737698a8aa4e5a4f6893bae467dad99152a7bb
francovegini <francovegini@hotmail.com>
2020-12-16 22:16:25 -0300

    Finalizado problema de montyhall

--- montyhall/src/App.vue
+++ montyhall/src/App.vue
@@ -6,6 +6,17 @@
         <label for="doorsAmount">Quantas portas?</label>
         <input type="text" id="doorsAmount" size="3" v-model.number="doorsAmount">
       </div>
+      <div v-if="!started">
+        <label for="selectedDoor">Qual porta é premiada?</label>
+        <input type="text" id="selectedDoor" size="3" v-model.number="selectedDoor">
+      </div>
+      <button v-if="!started" @click="started = true">Iniciar</button>
+      <button v-if="started" @click="started = false">Reiniciar</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in doorsAmount" :key="i">
+        <Door :hasGift="i === selectedDoor" :number="i"/>
+      </div>
     </div>
   </div>
 </template>
@@ -48,5 +59,25 @@ body {
   background-color: #0004;
   padding: 20px;
   margin-bottom: 60px;
+}
+
+.form{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+
+.form, .form input, .form button {
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+
+.doors {
+  align-items: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
